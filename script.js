e1d54974ff8637736dac94a546ef5744dd18e3b2
Carlos Alegria Moena <128553094+carlosalegriam@users.noreply.github.com>
2023-03-31 22:43:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function saludo() {
+  alert("Hola mundo");
+  var nombre = prompt("Ingrese su nombre: ");
+  alert("hola " + nombre);
+}
+
+var inicio = document.getElementsByClassName("link")[0];
+
+inicio.addEventListener("click", function() {
+  saludo();
+});
+
+function despedida() {
+  alert("Gracias por visitar la página");
+}
+
+var acerca = document.getElementsByClassName("link")[1];
+
+acerca.addEventListener("click", function() {
+  despedida();
+});
